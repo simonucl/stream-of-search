1dfc2cfa666e583b761ad62bebfadba9db475249
simon <simon011130@gmail.com>
2024-10-03 13:21:10 +0000

    local push

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-conda activate sos
+# conda activate sos
 
 cd src
 
-python eval_neo.py --ckpt checkpoint-500 -n 1000 -o 0 -d val1_b4_t100_n500000_random.json --temperature 0. --batch_size 64 --data_dir "" --gens 1
+# python eval_neo.py --ckpt checkpoint-500 -n 1000 -o 0 -d val1_b4_t100_n500000_random.json --temperature 0. --batch_size 64 --data_dir "" --gens 1
+
+python eval_neo_attention.py --ckpt simonycl/sos-sft-base -n 64 -o 0 -d val1_b4_t100_n500000_random.json --temperature 0. --batch_size 64 --data_dir "data/b4_3_random" --gens 1 --ctx 500
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-conda activate sos 
+# conda activate sos 
 
 cd src
 
-accelerate launch --config_file ../configs/accelerate.yaml train.py --config ../configs/sft-mix-4-cd5e5.conf
+accelerate launch --config_file ../configs/accelerate.yaml train.py --config ../configs/oft-mix-4-cd5e5.conf
